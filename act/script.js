bcb27ae8e6cbe10bfaa4b7abf76cb2b37f6d0035
mcswee <dj.mcsw.ee@gmail.com>
2025-12-29 19:03:10 +1100

    Update script.js

--- act/script.js
+++ act/script.js
@@ -1,4 +1,4 @@
-const map = L.map('state-report-map').setView([-35.2, 148], 7); // Centre of Aus
+const map = L.map('state-report-map').setView([-35.3, 149.1], 9);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; OpenStreetMap &copy; CartoDB',
@@ -13,29 +13,37 @@ let geoLayer = null;
 function loadGeoJSON(file) {
   fetch(file)
     .then(res => res.json())
-.then(function (data) {
-  if (geoLayer) {
-    map.removeLayer(geoLayer);
-  }
-
-  geoLayer = L.geoJSON(data, {
-    style: {
-      color: "#003895",
-      weight: 1
-    },
-    onEachFeature: function (feature, layer) {
-      if (feature.properties) {
-        var p = feature.properties;
-        var tooltip = "<b>" + (p.Elect_div || "Unknown Division") + "</b><br>" +
-                      "<strong>No of SA1:</strong> " + (p.Numccds || "—") + "<br>" +
-                      "<strong>Actual enrolment:</strong> " + (p.Actual || "—") + "<br>" +
-                      "<strong>Projected enrolment:</strong> " + (p.Projected || "—");
-        layer.bindTooltip(tooltip, { sticky: true });
+    .then(function (data) {
+      if (geoLayer) {
+        map.removeLayer(geoLayer);
       }
-    }
-  }).addTo(map);
 
- map.fitBounds(geoLayer.getBounds());
+      var mainlandBounds = L.latLngBounds();
+
+      geoLayer = L.geoJSON(data, {
+        style: {
+          color: "#003895",
+          weight: 1
+        },
+        onEachFeature: function (feature, layer) {
+          if (feature.properties) {
+            var p = feature.properties;
+            var tooltip = "<b>" + (p.Elect_div || "Unknown Division") + "</b><br>" +
+                          "<strong>No of SA1:</strong> " + (p.Numccds || "—") + "<br>" +
+                          "<strong>Actual enrolment:</strong> " + (p.Actual || "—") + "<br>" +
+                          "<strong>Projected enrolment:</strong> " + (p.Projected || "—");
+            layer.bindTooltip(tooltip, { sticky: true });
+          }
+
+          if (layer.getBounds().getWest() < 155) {
+             mainlandBounds.extend(layer.getBounds());
+          }
+        }
+      }).addTo(map);
+
+      if (mainlandBounds.isValid()) {
+          map.fitBounds(mainlandBounds, { padding: [30, 30] });
+      }
     })
     .catch(err => console.error(`Failed to load ${file}`, err));
 }
