8345a72401e06101a0d31292038aa0de8c4138c0
mcswee <dj.mcsw.ee@gmail.com>
2026-02-18 01:43:46 +1100

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -193,7 +193,11 @@ const popupContent = `
                 <strong>${data.winner_name || ''} ${(data.winner_surname || '').toUpperCase()}</strong>
                 <span class="party-pill">${(data.party || 'ind').toUpperCase()}</span>
             </div>
-            ${data.note ? `<small>${data.note}</small>` : ''}
+            ${data.note ? `
+                <small class="status-notice">
+                    ${data.note.split('|').join('<br>')}
+                </small>
+            ` : ''}
         </footer>
     </div>`;
 
