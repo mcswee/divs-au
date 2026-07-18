3fcb806a36defac7f3173e2578823d25732d7bc9
mcswee <dj.mcsw.ee@gmail.com>
2026-07-18 17:56:23 +1000

    Fix formatting of attribution string in setupMap

--- assets/js/tool.js
+++ assets/js/tool.js
@@ -88,7 +88,7 @@
   function setupMap() {
     map = L.map("tool-map", { preferCanvas: true });
     L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>|<a href="/copyright/>ABS & AEC</a>',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>|<a href="/copyright/">ABS & AEC</a>',
       maxZoom: 19
     }).addTo(map);
   }
